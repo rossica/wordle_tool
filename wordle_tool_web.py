efbdd04f58a9a6aa14353d29d92de2044caf233f
rossica <rossica@users.noreply.github.com>
2022-02-09 01:29:39 -0800

    Make the HTML centered and pretty.

--- wordle_tool_web.py
+++ wordle_tool_web.py
@@ -1,5 +1,5 @@
-from turtle import width
-from browser import document, alert, html
+from browser import document, alert
+from browser.html import *
 from urllib.request import urlopen
 from wordle_tool import IsLetter, sort_letter_position_stats, letter_position_stats, letter_stats_uniq, filter_words, filter_letters_known_position, filter_letter_unknown_position
 
@@ -19,7 +19,7 @@ def load_words():
 def load_button(event):
     load_words()
     global words
-    document['output'].innerHTML = 'Loaded {} words from dictionary file'.format(len(words))
+    update_log('', 'load')
 
 def print_button(event):
     document['output'].innerHTML = " ".join(words)
@@ -29,44 +29,61 @@ def print_stats_button(event):
     count = len(words)
     div = lambda x,y: x / y if y else 0
     stats = sort_letter_position_stats(letter_position_stats(words), letter_stats_uniq(words))
-    stats_text = "<table><tr><th>Letter</th><th>Frequency</th><th>1st Freq.</th><th>2nd Freq.</th><th>3rd Freq.</th><th>4th Freq.</th><th>5th Freq.</th><th>Multi</th></tr>"
+    table = TABLE()
+    table <= TR(TH('Letter') + TH('Frequency') + TH('1st Freq.') + TH('2nd Freq.') + TH('3rd Freq.') + TH('4th Freq.') + TH('5th Freq.') + TH('Multi'))
     for t in stats:
         if t[1] > 0:
-            stats_text += "<tr><td>{}</td><td>{:.2f}</td><td>{:.1f}</td><td>{:.1f}</td><td>{:.1f}</td><td>{:.1f}</td><td>{:.1f}</td><td>{}</td></tr>".format(t[0], (t[1]/count)*100, div(t[2][0], t[1])*100, div(t[2][1],t[1])*100, div(t[2][2],t[1])*100, div(t[2][3],t[1])*100, div(t[2][4],t[1])*100, "X" if sum(t[2]) > t[1] else '')
-    stats_text += "</table>"
-    document['stats'].innerHTML = stats_text
+            table <= TR(
+                TD(t[0]) +
+                TD('{:.2f}'.format((t[1]/count)*100)) +
+                TD('{:.1f}'.format(div(t[2][0], t[1])*100)) +
+                TD('{:.1f}'.format(div(t[2][1],t[1])*100)) +
+                TD('{:.1f}'.format(div(t[2][2],t[1])*100)) +
+                TD('{:.1f}'.format(div(t[2][3],t[1])*100)) +
+                TD('{:.1f}'.format(div(t[2][4],t[1])*100)) +
+                TD('X' if sum(t[2]) > t[1] else ''))
+    document['stats'].clear()
+    document['stats'] <= table
+    if document['divider'].hidden is True:
+        document['divider'].hidden = False
 
 def filter_incl_button(event):
     global words
     words = filter_words(words, document["cmd"].value)
-    document['output'].innerHTML = "{} words remaining".format(len(words))
+    update_log(document["cmd"].value, "incl")
+    print_stats_button(None)
+    print_button(None)
 
 def filter_excl_button(event):
     global words
     words = filter_words(words, document["cmd"].value, False)
-    document['output'].innerHTML = "{} words remaining".format(len(words))
+    update_log(document["cmd"].value, "excl")
+    print_stats_button(None)
+    print_button(None)
 
 def filter_pos_known(event):
     global words
     words = filter_letters_known_position(words, document["cmd"].value)
-    document['output'].innerHTML = "{} words remaining".format(len(words))
+    update_log(document["cmd"].value, "fpk")
+    print_stats_button(None)
+    print_button(None)
 
 def filter_pos_unknown(event):
     global words
     words = filter_letter_unknown_position(words, document["cmd"].value)
-    document['output'].innerHTML = "{} words remaining".format(len(words))
+    update_log(document["cmd"].value, "fpu")
+    print_stats_button(None)
+    print_button(None)
 
 def adv_help_button(event):
     help_text = """
     Load                    - Loads dictionary file
-    Print                   - Shows all filtered words
-    Print stats             - Prints stats for filtered words
     Filter (incl.)          - Filters to words containing letters
     Filter (excl.)          - Filters out words containing letters
     Filter position known   - Filters to words with letters in given position.
         e.g. __a__. Allows multiple letters, e.g. ___ly.
-    Filter position unknown - Filter to words with letter not in position
-        e.g. _b___. Only one letter at a time
+    Filter position unknown - Filters to words with letters *not* in position.
+        e.g. _b___. Allows multiple letters, e.g. a_l__.
     """
     alert(help_text)
 
@@ -78,22 +95,19 @@ def text_click(event):
     else:
         event.currentTarget.class_name = INPUT_BLACK
 
-def update_log():
+def update_log(cmd=None, op=None):
     document['log'].hidden = False
-    t = html.TABLE()
-    t <= html.TR(
-        html.TD(document['one'].value, Class=document['one'].class_name) +
-        html.TD(document['two'].value, Class=document['two'].class_name) +
-        html.TD(document['three'].value, Class=document['three'].class_name) +
-        html.TD(document['four'].value, Class=document['four'].class_name) +
-        html.TD(document['five'].value, Class=document['five'].class_name))
-    document['log'] <= t
-    # document['log'] <= html.DIV(document['one'].value, Class=document['one'].class_name, width="1em")
-    # document['log'] <= html.DIV(document['two'].value, Class=document['two'].class_name, width="1em")
-    # document['log'] <= html.DIV(document['three'].value, Class=document['three'].class_name, width="1em")
-    # document['log'] <= html.DIV(document['four'].value, Class=document['four'].class_name, width="1em")
-    # document['log'] <= html.DIV(document['five'].value, Class=document['five'].class_name, width="1em")
-
+    if document['simple_controls'].hidden == True:
+        document['log'] <= P("{} {} {}".format(cmd, "|" if cmd else '', op))
+    else:
+        t = TABLE()
+        t <= TR(
+            TD(document['one'].value, Class=document['one'].class_name) +
+            TD(document['two'].value, Class=document['two'].class_name) +
+            TD(document['three'].value, Class=document['three'].class_name) +
+            TD(document['four'].value, Class=document['four'].class_name) +
+            TD(document['five'].value, Class=document['five'].class_name))
+        document['log'] <= t
 
 def get_letter(elem, excl, fpu, fpk):
     letter = elem.value.upper()
@@ -137,8 +151,7 @@ def simple_filter_button(event):
             extras.append(l)
         if l in fpk:
             extras.append(l)
-    if len(extras) > 0:
-        excl.extend(extras)
+    excl.extend(extras)
 
     if len(excl) != 0:
         temp = filter_words(words, "".join(excl), False)
@@ -154,22 +167,30 @@ def simple_filter_button(event):
     print_button(None)
 
 def simple_help_button(event):
-    pass
+    simple_help_text = """
+    Enter the output from Wordle(tm) into the boxes.
+    Click on each box to change the color to yellow, green, or black.
+    Click on "Filter" to get the output.
+    """
+    alert(simple_help_text)
 
 def show_adv_controls_button(event):
-    document['simple_input'].hidden = True
-    document['advanced_input'].hidden = False
+    document['simple_controls'].hidden = True
+    document['advanced_controls'].hidden = False
+
+def show_simple_controls_button(event):
+    document['simple_controls'].hidden = False
+    document['advanced_controls'].hidden = True
 
 # On page load, the following is executed
 words = list()
 document['load_button'].bind("click", load_button)
-document['print_button'].bind("click", print_button)
-document['print_stats_button'].bind("click", print_stats_button)
 document['filter_incl_button'].bind("click", filter_incl_button)
 document['filter_excl_button'].bind("click", filter_excl_button)
 document['filter_pos_known_button'].bind("click", filter_pos_known)
 document['filter_pos_unknown_button'].bind("click", filter_pos_unknown)
 document['adv_help_button'].bind("click", adv_help_button)
+document['show_simple_controls_button'].bind("click", show_simple_controls_button)
 
 document['one'].bind("click", text_click)
 document['two'].bind("click", text_click)
